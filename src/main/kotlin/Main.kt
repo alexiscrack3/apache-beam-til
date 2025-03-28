6bed9ff8a79cf8ff52a387a35b08fe03d7c3a490
Alexis Ortega <alexis.oc3@gmail.com>
2025-03-28 16:27:28 -0600

    Run in-memory collection

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,54 @@
 package org.alexiscrack3
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.options.PipelineOptionsFactory
+import org.apache.beam.sdk.transforms.Create
+import org.apache.beam.sdk.transforms.DoFn
+import org.apache.beam.sdk.transforms.ParDo
+import org.apache.beam.sdk.values.PCollection
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    inMemoryCollection()
+}
+
+fun inMemoryCollection() {
+    val options = PipelineOptionsFactory.create()
+    val pipeline: Pipeline = Pipeline.create(options)
+
+    // Now create the PCollection using list of strings
+    val words: PCollection<String> =
+        pipeline.apply(
+            Create.of("To", "be", "or", "not", "to", "be", "that", "is", "the", "question")
+        )
+
+
+    // Create a numerical PCollection
+    val numbers: PCollection<Int> =
+        pipeline.apply(
+            Create.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        )
+    words.apply("Print words", ParDo.of(PrintWords()))
+    numbers.apply("Print numbers", ParDo.of(PrintIntegers()))
+
+    pipeline.run().waitUntilFinish()
+}
+
+class PrintWords: DoFn<String, String>() {
+    @ProcessElement
+    @Throws(Exception::class)
+    fun processElement(c: ProcessContext) {
+        val word = c.element()
+        println(word)
+        c.output(word)
+    }
+}
+
+class PrintIntegers: DoFn<Int, Int>() {
+    @ProcessElement
+    @Throws(Exception::class)
+    fun processElement(c: ProcessContext) {
+        val number = c.element()
+        println(number)
+        c.output(number)
     }
 }
